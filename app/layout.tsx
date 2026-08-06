337efad994d4f37c8932ba536ec26641693b7e49
Shaun Tshimangadzo Madabeni <122936635+Shaun2003@users.noreply.github.com>
2026-08-06 14:35:49 +0200

    Remove AdSense component from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Poppins } from 'next/font/google'
 import { SiteHeader } from '@/components/site-header'
 import { SiteFooter } from '@/components/site-footer'
 import { JsonLd } from '@/components/json-ld'
-import { AdSense } from '@/components/adsense'
 import { organizationSchema, websiteSchema } from '@/lib/seo'
 import './globals.css'
 
@@ -104,7 +103,6 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased">
-        <AdSense />
         <SiteHeader />
         <main>{children}</main>
         <SiteFooter />
